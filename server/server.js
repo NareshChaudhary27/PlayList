3ed2d9bd446c07bb392ce6bb77235807260732a3
Naresh Kumar <chaudharynaresh2705@gmail.com>
2025-05-22 12:20:11 +0530

    First commit: Database connection

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
